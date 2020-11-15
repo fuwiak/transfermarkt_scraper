3887a727944be3dbfd9a62a2159b40e1c9e7a1d9
Macbook SSD <poczta130@gmail.com>
2020-11-15 23:33:38 +0000

    opera driver

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import pandas as pd
 import numpy as np
+# Importing options for the Opera module
+
+from webdriver_manager.opera import OperaDriverManager
 
 url = "https://www.transfermarkt.com/robert-lewandowski/profil/spieler/38253"
 
 from selenium import webdriver
 
-driver = webdriver.Chrome()
 
+# https://github.com/operasoftware/operachromiumdriver/releases
+
+# driver = webdriver.Opera()
+
+
+driver = webdriver.Opera(OperaDriverManager().install())
 playerPage = driver.get(url)
 df = pd.read_html(driver.page_source)[0]
 
